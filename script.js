e63c7dbb354569b3cb69c20513079cd90c8736b8
MsJamesM <118479305+MsJamesM@users.noreply.github.com>
2023-05-22 17:35:32 -0600

    added functionality to application in JS file. Tweaked HTML and CSS files

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  return "new password here"
+function generate() {
+  var length = document.getElementById('length').value;
+  var numbers = document.getElementById('numbers').checked;
+  var nocaps = document.getElementById('nocaps').checked;
+  var caps = document.getElementById('caps').checked;
+  var symbols = document.getElementById('symbols').checked;
+  
+  var password = generatePassword(parseInt(length), numbers, caps, nocaps, symbols);
+  document.getElementById('displayArea').innerHTML = password;
+  
+function generatePassword(length, numbers, caps, nocaps, symbols) {
+  let characters = '';
+
+  if (caps) {
+    characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (nocaps) {
+    characters += "abcdefghijklmnopqrstuvwxyz";
+    }
+    if (numbers) {
+    characters += "0987654321";
+  }
+  if (symbols) {
+    characters += "!@#$%^&*()";
+  }
+
+  if (length > maxLength) {
+  }
+
+
+  let passwordArray = [];
+  while (passwordArray.length < length) {
+    var character = characters[Math.floor(Math.random() * characters.length)];
+    passwordArray.push(character);
+    }
+  return passwordArray.join('');
 }
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
